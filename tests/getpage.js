ad246a865dc5220520fa8700f575de15245967cd
Paul Mackay <pauljmackay@gmail.com>
2015-03-23 16:47:15 +0000

    phantomjs through ansible.

--- tests/getpage.js
+++ tests/getpage.js
@@ -1,7 +1,13 @@
 
+var system = require('system');
+var args = system.args;
+
 console.log('Loading a web page');
 var page = require('webpage').create();
-var url = 'https://openfoodnetwork.org.au';
+
+// Get URL from command line
+var url = args[1];
+
 page.open(url, function (status) {
   var title = page.evaluate(function() {
     return document.title;
